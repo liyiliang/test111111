8051e81c487fad3d8273787cf8c13f202594ca17
v_liliangli(李亮) <v_liliangli@tencent.com>
2016-09-05 17:41:57 +0800

    JZ-002

--- 01.php
+++ 01.php
@@ -4,7 +4,15 @@ echo '1233';
 
 $link=mysql_connect('localhost','root','');
 
+echo 'test';
+
 if($link)
 {
-	echo "connection OK";
+	echo "connection success OK";
 }
+
+mysql_select_db('test');
+
+$sql="show tables";
+mysql_query($sql);
+
